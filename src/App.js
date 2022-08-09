3c95f38855e689416a87befadb2f0164b92a26ad
Evomatic <evantrujillo30@gmail.com>
2022-08-09 23:56:33 +0900

    simple styles for container | added component files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     placeholder
-    </div>
+    <Container className="card-container" maxWidth="lg">
+      <Box className="box"></Box>
+    </Container>
   );
 }
 
