fab7cb93d9ce4a23ca4ada6c827e1be5cf391d06
Evomatic <evantrujillo30@gmail.com>
2022-08-14 15:31:03 +0900

    restructured app for MUI use

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
-import './App.css';
 
 function App() {
   return (
-    <Container className="card-container" maxWidth="lg">
-      <Box className="box"></Box>
+    <Container className="container" maxWidth="lg" sx={{
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      backgroundImage: "url(./assets/bg-main-mobile.png)"
+    }}>
+      <Box className="box" sx={{width: "100%", height: "100%", backgroundColor: "black"}}></Box>
     </Container>
   );
 }
