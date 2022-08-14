975831d72b126a1c777304ab8172b13172c706d1
Evomatic <evantrujillo30@gmail.com>
2022-08-14 22:21:42 +0900

    added form detail fields

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import FormDetailFields from './components/FormDetailFields';
 import './App.css';
 
 function App() {
   return (
     <Container className="container" maxWidth="false" >
-      <Box className="box-wrapper"></Box>
+      <Box className="box-wrapper">
+        <FormDetailFields className="form-detail-fields" />
+      </Box>
     </Container>
   );
 }
---
+++ src/components/FormDetailFields.jsx
@@ -0,0 +1,15 @@
+import TextField from '@mui/material/TextField';
+import FormControl from '@mui/material/FormControl';
+import './FormDetailFields.css';
+
+
+
+export default function FormDetailFields() {
+   return (
+    <FormControl>
+      <TextField />
+      <TextField />
+      <TextField />
+    </FormControl>
+   )
+}
