1ffde39a904d411a5e4767b78bca1b55c510e023
Evomatic <evantrujillo30@gmail.com>
2022-08-17 20:56:00 +0900

    further improvements

--- src/components/FormDetailFields.jsx
+++ src/components/FormDetailFields.jsx
@@ -1,15 +1,17 @@
 import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
 import TextField from '@mui/material/TextField';
 import './FormDetailFields.css';
-
 
 
 export default function FormDetailFields() {
    return (
     <FormControl className="form">
+      <InputLabel htmlfor="TextField">test</InputLabel>
       <TextField className="name-field"/>
       <TextField  className="number-field"/>
-      <TextField className="date-field" />
+      <TextField className="date-fields"/>
+      <TextField className="date-fields"/>
     </FormControl>
    );
 }
