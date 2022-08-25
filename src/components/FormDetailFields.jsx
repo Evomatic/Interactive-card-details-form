10af38008ba94d05c0ebc9a96266b3a6fdf956bd
Evomatic <evantrujillo30@gmail.com>
2022-08-25 23:51:41 +0900

    added styling to form fields

--- src/components/FormDetailFields.jsx
+++ src/components/FormDetailFields.jsx
@@ -7,9 +7,9 @@ import './FormDetailFields.css';
 export default function FormDetailFields() {
    return (
     <FormControl className="form">
-      <TextField className="name-field"/>
-      <TextField  className="number-field"/>
-      <TextField className="date-field" />
+      <TextField className="form-field name" placeholder="e.g. Jane Appleseed"/>
+      <TextField className="form-field number" placeholder="e.g. 1234 5678 9123 0000"/>
+      <TextField className="form-field date" />
     </FormControl>
    );
 }
