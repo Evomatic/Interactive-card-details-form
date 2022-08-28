0a806236103767485a6e6673ad87f3eb135da94f
Evomatic <evantrujillo30@gmail.com>
2022-08-28 16:10:39 +0900

    changes to fields

--- src/components/FormDetailFields.jsx
+++ src/components/FormDetailFields.jsx
@@ -1,13 +1,24 @@
+import { Fragment } from 'react';
 import FormControl from '@mui/material/FormControl';
 import TextField from '@mui/material/TextField';
 import './FormDetailFields.css';
 
 export default function FormDetailFields() {
+
+  function dateFields() {
+    return (
+      <Fragment>
+        <TextField className="form-field month" />
+        <TextField className="form-field year" />
+        <TextField className="form-field cvc" />
+      </Fragment>
+    )
+  }
    return (
     <FormControl className="form">
       <TextField className="form-field name" placeholder="e.g. Jane Appleseed"/>
       <TextField className="form-field number" placeholder="e.g. 1234 5678 9123 0000"/>
-      <TextField className="form-field date" />
+      {dateFields()}
     </FormControl>
    );
 }
