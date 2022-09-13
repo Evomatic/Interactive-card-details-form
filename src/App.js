594c5c81d5a318451cbb01cbe96f2ffd1e80be60
Evomatic <evantrujillo30@gmail.com>
2022-09-13 23:55:18 +0900

    refactored file structure to be more not as complex

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-import FormDetailFields from './components/FormDetailFields';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
 import './App.css';
 
 function App() {
   return (
     <Container className="container" maxWidth="false" >
       <Box className="box-wrapper">
-        <FormDetailFields className="form-detail-fields" />
+      <FormControl className="form">
+      <TextField className="form-field name" placeholder="e.g. Jane Appleseed"/>
+      <TextField className="form-field number" placeholder="e.g. 1234 5678 9123 0000"/>
+        <TextField className="form-field month" placeholder="MM" />
+        <TextField className="form-field year" placeholder="YY" />
+        <TextField className="form-field cvc" placeholder="e.g. 123"/>
+    </FormControl>
       </Box>
     </Container>
   );
