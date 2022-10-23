0486acd532ffbf4d0022c6b5a05a57f1bf1927d1
Evomatic <evantrujillo30@gmail.com>
2022-10-23 15:47:05 +0900

    added css for field labels

--- src/App.js
+++ src/App.js
@@ -12,13 +12,17 @@ function App() {
      <FormControl className="form">
        <InputLabel className="name-label" size="small" >CARDHOLDER NAME</InputLabel>
        <TextField className="form-field name" placeholder="e.g. Jane Appleseed"/>
+       <InputLabel className="number-label" size="small" >CARD NUMBER</InputLabel>
        <TextField className="form-field number" placeholder="e.g. 1234 5678 9123 0000"/>
       <Box className="field-group">
-        <TextField className="form-field month" placeholder="MM" />
+        <Box className=" credit-card-expiration-box">
+          <InputLabel className="month-label" size="small" >EXP. DATE</InputLabel>
+          <TextField className="form-field month" placeholder="MM" />
+        </Box>
         <TextField className="form-field year" placeholder="YY" />
         <TextField className="form-field cvc" placeholder="e.g. 123"/>
       </Box>
-      <Button variant="contained" size="large">Confirm</Button>
+      <Button className="confirm-btn" type ="submit" variant="contained" size="large">Confirm</Button>
      </FormControl>
    </Container>
   );
