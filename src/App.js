a2e57a5ba89785ba21b6504cd5bd1cc4ad5d9764
Evomatic <evantrujillo30@gmail.com>
2022-10-31 18:33:02 +0900

    updated global font to use

--- src/App.js
+++ src/App.js
@@ -16,7 +16,10 @@ function App() {
      </Box>
      <FormControl className="form">
        <InputLabel className="name-label" size="small" >CARDHOLDER NAME</InputLabel>
-       <TextField className="form-field name" placeholder="e.g. Jane Appleseed"/>
+       <TextField 
+         className="form-field name" 
+         placeholder="e.g. Jane Appleseed" 
+         sx={{ "& .MuiOutlinedInput-root": { borderRadius: "50px"}}} />
        <InputLabel className="number-label" size="small" >CARD NUMBER</InputLabel>
        <TextField className="form-field number" placeholder="e.g. 1234 5678 9123 0000"/>
       <Box className="field-group">
--- src/theme.js
+++ src/theme.js
@@ -3,6 +3,10 @@ import { createTheme } from '@mui/material/styles';
 
 // A custom theme for this app
 const theme = createTheme({
+  typography: {
+    fontFamily: ["Space Grotesk", "sans-serif"].join(","),
+    fontSize: "18px",
+  },
   palette: {
     primary: {
       main: '#556cd6',
