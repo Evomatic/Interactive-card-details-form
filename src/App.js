2cdf655ec3dcaf1cc076d75501aed0ed3308c9e1
Evomatic <evantrujillo30@gmail.com>
2022-10-31 13:03:15 +0900

    better styling for field group

--- src/App.js
+++ src/App.js
@@ -15,12 +15,18 @@ function App() {
        <InputLabel className="number-label" size="small" >CARD NUMBER</InputLabel>
        <TextField className="form-field number" placeholder="e.g. 1234 5678 9123 0000"/>
       <Box className="field-group">
-        <Box className=" credit-card-expiration-box">
-          <InputLabel className="month-label" size="small" >EXP. DATE</InputLabel>
+        <Box className="expiration-box">
+          <InputLabel className="exp-label" size="small" >EXP. DATE</InputLabel>
           <TextField className="form-field month" placeholder="MM" />
         </Box>
-        <TextField className="form-field year" placeholder="YY" />
-        <TextField className="form-field cvc" placeholder="e.g. 123"/>
+        <Box className="mm-yy-box">
+          <InputLabel className="mm-yy-label" size="small" >(MM/YY)</InputLabel>
+          <TextField className="form-field year" placeholder="YY" />
+        </Box>
+        <Box className="cvc-box">
+          <InputLabel className="cvc-label" size="small" >CVC</InputLabel>
+          <TextField className="form-field cvc" placeholder="e.g. 123"/>
+        </Box>
       </Box>
       <Button className="confirm-btn" type ="submit" variant="contained" size="large">Confirm</Button>
      </FormControl>
