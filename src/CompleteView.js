8d74ffe97032fd82778f060ec21c55652bb7bf66
Evomatic <evantrujillo30@gmail.com>
2022-12-10 13:59:30 +0900

    Create simple complete view and add css file

--- src/CompleteView.js
+++ src/CompleteView.js
@@ -1,7 +1,17 @@
+import './CompleteView.css';
+
 import { Box } from '@mui/material';
 
+import completeIcon from './assets/icon-complete.svg';
+
 function CompleteView() {
-  return <Box>THANK YOU!</Box>;
+  return (
+    <Box className="complete-view-container">
+      <img className="complete-icon" src={completeIcon} alt="complete icon" />
+      <span className="thank-you-text">THANK YOU!</span>
+      <span className="descriptive-text">We&apos;ve added your card details</span>
+    </Box>
+  );
 }
 
 export default CompleteView;
